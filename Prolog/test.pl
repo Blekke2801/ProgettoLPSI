c890ac8a6a6f711df07d8a8d05d79a7d316cca0d
Blekke <83006362+Blekke2801@users.noreply.github.com>
2023-09-21 21:31:46 +0200

    funzionano tutti i test porcaccia la?

--- Prolog/test.pl
+++ Prolog/test.pl
@@ -18,8 +18,53 @@ test_dimensione_maz4 :-
 test_dimensione_ma24 :- 
     is_dimension(m*('A'**2)**4).
 
+test_dimensione_mBqs :-
+    is_dimension(m * 'Bq' * s).
+
+test_symbol_metre :-
+    siu_symbol(metre, m).
+
+test_name_m :-
+    siu_name(m, metre).
+
+test_name_Bq :-
+    siu_name('Bq', 'Becquerel').
+
+test_compare_mm :-
+    compare_units(=,m,m).
+
+test_compare_aa :-
+    not(compare_units(_,a,a)).
+
+test_compare_mkg :-
+    compare_units(<,m,kg).
+
+test_compare_mBq :-
+    compare_units(>,m,'Bq').
+
 test_qtimes :- 
     qtimes(q(42, m), q(0.5, (s ** 3) * (m ** -3)), q(21.0 , (m ** -2) * (s ** 3))).
+
+test_qtimes_quad :-
+    qtimes(q(42,m),q(0.5,m), q(21.0, m**2)).
+
+test_qdiv_normal :- 
+    qdiv(q(21,m),q(0.5,s),q(42.0, m*s** -1)).
+
+test_qdiv_per0 :-
+    not(qdiv(q(21,m),q(0,s),_)).
+
+test_qdiv_nonquantity :-
+    not(qdiv(q(21,a),q(0.5,s),_)).
+
+test_qexpt_normale :-
+    qexpt(q(2,m),4,q(16, m**4)).
+
+test_qexpt_conexp :-
+    qexpt(q(2,m**2),4,q(16, m**8)).
+
+test_qexpt_false :-
+    not(qexpt(q(2,a**2),3,_)).
 
 test_qsum_false :- 
     not(qsum(q(42, m), q(0.5, (s ** 3) * (m ** -3)), _)).
@@ -34,12 +79,32 @@ test_norm :-
     norm((m ** -2) * 'A' * (s ** -2) * kg * (m ** 2) * ('K' ** 2) * (s ** -1), kg * (s ** -3) * 'A' * ('K' ** 2)).
 
 test_qsum_prefixes :- 
-    qsum(q(2, m), q(20, cm), q(2.2, m)).
+    qsum(q(42, m), q(42, cm), q(42.42, m)).
+
+test_qsum_false_2 :- 
+    not(qsum(q(42,ms), q(42,kg) , _)).
+
+test_qsum_ms :-
+    qsum(q(42,ms), q(42,ms), q(84, ms)).
+
+test_qsub_negative :-
+    not(qsub(q(42,m), q(44,m), _)).
+
+test_qsub_normal :-
+    qsub(q(42,ms), q(42,ms),q(0, ms)).
+
+test_qsub_negative_prefixes :-
+    not(qsub(q(42,cm), q(44,m),_)).
+
 
 test_once(Predicate) :-
     write(Predicate),
     call(Predicate),
-    write(": ✅\n").
+    !,
+    write(": 'V'\n").
+
+test_once(_) :-
+    write(": 'X'\n").
 
 test_all :-
     test_once(test_dimensione_m),
@@ -48,12 +113,33 @@ test_all :-
     test_once(test_dimensione_ma),
     test_once(test_dimensione_maz4),
     test_once(test_dimensione_ma24),
+    test_once(test_dimensione_mBqs),
+    test_once(test_symbol_metre),
+    test_once(test_name_m),
+    test_once(test_name_Bq),
+    test_once(test_compare_mm),
+    test_once(test_compare_aa),
+    test_once(test_compare_mkg),
+    test_once(test_compare_mBq),
     test_once(test_qtimes),
-    test_once(test_qsum_false),
+    test_once(test_qtimes_quad),
+    test_once(test_qdiv_normal),
+    test_once(test_qdiv_per0),
+    test_once(test_qdiv_nonquantity),
+    test_once(test_qexpt_normale),
+    test_once(test_qexpt_conexp),
+    test_once(test_qexpt_false),
     test_once(test_symbol_ohm),
     test_once(test_symbol_dc),
     test_once(test_norm),
-    test_once(test_qsum_prefixes).
+    test_once(test_qsum_prefixes),
+    test_once(test_qsum_false),
+    test_once(test_qsum_ms),
+    test_once(test_qsub_negative),
+    test_once(test_qsub_normal),
+    test_once(test_qsub_negative_prefixes),
+    test_once(test_qsum_false_2).
+    
 
 reloadt :- [test], tty_clear.
     
