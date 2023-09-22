f146b48ab3e6d7873ef53f34653b84bb8b5a0cf5
Blekke <83006362+Blekke2801@users.noreply.github.com>
2023-09-22 20:08:58 +0200

    test

---
+++ Lisp/test.lisp
@@ -0,0 +1,132 @@
+(defun test-dimensione-m ()
+  (is-dimension 'm))
+
+(defun test-dimensione-ms ()
+  (is-dimension '(* m s)))
+
+(defun test-dimensione-sm ()
+  (is-dimension '(* s m)))
+
+(defun test-dimensione-ma ()
+  (is-dimension '(* m A)))
+
+(defun test-dimensione-maz4 ()
+  (not (is-dimension '(* m A (** z 4)))))
+
+(defun test-dimensione-ma24 ()
+  (is-dimension '(* m (** (** A 2) 4))))
+
+(defun test-dimensione-mBqs ()
+  (is-dimension '(* m Bq s)))
+
+(defun test-symbol-metre ()
+  (equal (siu-symbol 'metre) 'm))
+
+(defun test-name-m ()
+  (equal (siu-name 'm) 'metre))
+
+(defun test-name-Bq ()
+  (equal (siu-name 'Bq) 'Becquerel))
+
+(defun test-compare-mm ()
+  (equal (compare-units 'm 'm) '=))
+
+(defun test-compare-mkg ()
+  (equal (compare-units 'm 'kg) '<))
+
+(defun test-compare-mBq ()
+  (equal (compare-units 'm 'Bq) '>))
+
+(defun test-qtimes ()
+  (equal (qtimes (q 42 'm) (q 0.5 '(* (** s 3) (** m -3)))) (q 21.0 '(* (** m -2) (** s 3)))))
+
+(defun test-qtimes-quad ()
+  (equal (qtimes (q 42 'm) (q 0.5 'm)) (q 21.0 '(** m 2))))
+
+(defun test-qdiv-normal ()
+  (equal (qdiv (q 21 'm) (q 0.5 's)) (q 42.0 '(* m (** s -1)))))
+
+(defun test-qdiv-per0 ()
+  (stringp (qdiv (q 21 'm) (q 0 's))))
+
+(defun test-qdiv-nonquantity ()
+  (stringp (qdiv (q 21 'a) (q 0.5 's))))
+
+(defun test-qexpt-normale ()
+  (equal (qexpt (q 2 'm) 4) (q 16 '(** m 4))))
+
+(defun test-qexpt-conexp ()
+  (equal (qexpt (q 2 '(** m 2)) 4) (q 16 '(** m 8))))
+
+(defun test-qexpt-false ()
+  (stringp (qexpt (q 2 '(** a 2)) 3)))
+
+(defun test-qsum-false ()
+  (stringp (qsum (q 42 'm) (q 0.5 '(* (** s 3) (** m -3))))))
+
+(defun test-symbol-ohm ()
+  (equal (siu-symbol 'ohm) 'Omega))
+
+(defun test-symbol-dc ()
+  (equal (siu-symbol 'degreecelsius) 'dc))
+
+(defun test-norm ()
+  (equal (norm '(* (** m -2) A (** s -2) kg (** m 2) (** K 2) (** s -1))) '(* kg (** s -3) A (** K 2))))
+
+(defun test-qsum-prefixes ()
+  (equal (qsum (q 42 'm) (q 42 'cm)) (q 42.42 'm)))
+
+(defun test-qsum-false-2 ()
+  (stringp (qsum (q 42 'ms) (q 42 'kg))))
+
+(defun test-qsum-ms ()
+  (equal (qsum (q 42 'ms) (q 42 'ms)) (q 84 'ms)))
+
+(defun test-qsub-negative ()
+  (stringp (qsub (q 42 'm) (q 44 'm))))
+
+(defun test-qsub-normal ()
+  (equal (qsub (q 42 'ms) (q 42 'ms)) (q 0 'ms)))
+
+(defun test-qsub-negative-prefixes ()
+  (stringp (qsub (q 42 'cm) (q 44 'm))))
+
+(defun test-once (predicate)
+  (format t "~A" predicate)
+  (if (funcall predicate)
+    (format t ": 'V~%")
+    (format t ": 'X~%")))
+
+(defun test-all ()
+  (test-once #'test-dimensione-m)
+  (test-once #'test-dimensione-ms)
+  (test-once #'test-dimensione-sm)
+  (test-once #'test-dimensione-ma)
+  (test-once #'test-dimensione-maz4)
+  (test-once #'test-dimensione-ma24)
+  (test-once #'test-dimensione-mBqs)
+  (test-once #'test-symbol-metre)
+  (test-once #'test-name-m)
+  (test-once #'test-name-Bq)
+  (test-once #'test-compare-mm)
+  (test-once #'test-compare-mkg)
+  (test-once #'test-compare-mBq)
+  ; (test-once #'test-qtimes)
+  ; (test-once #'test-qtimes-quad)
+  ; (test-once #'test-qdiv-normal)
+  ; (test-once #'test-qdiv-per0)
+  ; (test-once #'test-qdiv-nonquantity)
+  ; (test-once #'test-qexpt-normale)
+  ; (test-once #'test-qexpt-conexp)
+  ; (test-once #'test-qexpt-false)
+  (test-once #'test-symbol-ohm)
+  (test-once #'test-symbol-dc)
+  ; (test-once #'test-norm)
+  ; (test-once #'test-qsum-prefixes)
+  ; (test-once #'test-qsum-false)
+  ; (test-once #'test-qsum-ms)
+  ; (test-once #'test-qsub-negative)
+  ; (test-once #'test-qsub-normal)
+  ; (test-once #'test-qsub-negative-prefixes)
+  ; (test-once #'test-qsum-false-2)
+  )
